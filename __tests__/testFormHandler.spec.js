e9182a76804bbe5a5446ddd22923727ba38c533f
TANADADaiske <nvkx873110@gmail.com>
2022-01-15 06:43:02 +0900

    feat: add test

---
+++ __tests__/testFormHandler.spec.js
@@ -0,0 +1,7 @@
+import { formHandler } from '../src/client/js/app';
+
+describe("Testing the submit functionality", () => {
+    test("Testing the formHander() function", () => {
+        expect(formHandler).toBeDefined();
+    })
+});
